641b8f10660ed85c0bc45d39743d4170f4562f46
ASROOPA <34640686+ASROOPA@users.noreply.github.com>
2018-01-09 16:03:16 +0530

    Update Bitwise and boolean algebra.c

--- BEGINNER/Bitwise and boolean algebra.c
+++ BEGINNER/Bitwise and boolean algebra.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 #include<string.h>
+#include<math.h>
 int main(void) {
-	int n=15,i=0,a[10],j,remainder,count=0,binary=0,base=1;
-	char ch=ch+n;
-while (ch > 0)
+	int n,i=0,a[10],j,remainder,count=0,binary=0,base=1,length,flag=0,len;
+	char ch[10]="372";
+	len=strlen(ch);
+	for(i=0;i<len;i++)
+	{
+		if(ch[i]=='.'){
+			flag++;
+		}
+	}
+	if(flag==0)		
+	{
+	n=atoi(ch);	
+		while(n!=0)
     {
-        remainder = ch % 2;
+        remainder = n % 2;
         binary = binary + remainder * base;
-        ch = ch / 2;
+        n = n / 2;
         base = base * 10;
     }
     printf("Its binary equivalent is = %ld\n", binary);
+	}
+	else
+	{
+		printf("error");
+	}
 	return 0;
 }
